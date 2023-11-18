fb9914e53986d6bc007903ce53f52a52db2cb478
Pauras22 <paurastarle22@gmail.com>
2023-11-18 16:30:17 +0530

    some updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
